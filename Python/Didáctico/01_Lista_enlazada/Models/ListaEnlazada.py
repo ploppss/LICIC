9bf8b0de0552a8423d00595a924b473faff4f278
ploppss <josealgomgar007@gmail.com>
2026-04-07 21:55:08 -0600

    Lista enlazada mejorada en Python

--- Python/Didáctico/01_Lista_enlazada/Models/ListaEnlazada.py
+++ Python/Didáctico/01_Lista_enlazada/Models/ListaEnlazada.py
@@ -6,29 +6,43 @@ class ListaEnlazada:
     """
 
     def __init__(self):
-        self.head = None
+        self.__head = None # Privatizacion del atributo head
     
-    def insertar_final(self, valor):
+    def insertar_inicio(self, valor):
+        """
+            Este método inserta siempre un nuevo nodo con el valor que se le pase al inicio de la lista
+        Args:
+            valor (_type_): Cualquier valor que se le desee asignar al nuevo nodo
+        """
+        nuevo_nodo = Nodo(valor)
+        nuevo_nodo.ptr = self.__head
+        self.__head = nuevo_nodo
         
-        if self.head is None:
-            self.head = nuevo_nodo
+    def insertar_final(self, valor):
+        """
+            Este método se encarga de recorrer la lista enlazada para llegar al final e insertar un nuevo elemento, evalua si la lista esta vacía y si esta vacía
+            llama al método insertar_inicio.
+        Args:
+            valor (_type_): Cualquier valor que se le desee asignar al nuevo nodo
+        """
+        if self.__head is None:
+            self.insertar_inicio(valor)
         else:
             nuevo_nodo = Nodo(valor)
-            iterador = self.head
+            iterador = self.__head
             while iterador.ptr is not None:
                 iterador = iterador.ptr
             iterador.ptr = nuevo_nodo
-  
-    def insertar_inicio(self, valor):
-        
-        if self.head is None:
-            self.insertar_final(valor)
-        else:
-            nuevo_nodo = Nodo(valor)
-            nuevo_nodo.ptr = self.head
-            self.head = nuevo_nodo
-            
+              
     def insertar_indexado(self, valor, pos):
+        """
+            Este método inserta un nuevo nodo en una posición deseada, primero se le pasa el valor que se inserta y luego la posicion donde se desea insertar
+            si la posicion es 0 se llama al método insertar_inicio, si la posicion que se le pasa es mayor a uno el método procede a buscar la posición con un iterador (i)
+            y un ciclo while
+        Args:
+            valor (_type_): El valor que se le da al nuevo nodo
+            pos (_type_): La posición en la que se desea insertar
+        """
         if pos <= 0:
             print(f"ERROR. Por favor inserte una posición mayor a 0")
             return
@@ -37,7 +51,7 @@ class ListaEnlazada:
             self.insertar_inicio(valor)
         else:
             i = 0
-            iterador = self.head
+            iterador = self.__head
             while i < pos - 1 and iterador is not None:
                 iterador = iterador.ptr
                 i += 1 
@@ -49,17 +63,25 @@ class ListaEnlazada:
                 print(f"ERROR. Posición fuera de alcance, el tamaño de la lista es de {i} elementos")
                       
     def eliminar_indexado(self, pos):
+        """
+            Este método solo necesita recibir una posicion para eliminar el nodo de esa posición, si recibe la primera posición entra a un bloque if donde guarda la 
+            dirección donde apunta la cabeza en una referencia temporal, luego a la cabeza se le asigna la referencia que guarda el nodo al que apuntaba la cabeza.
+            Para finalizar se borra la referencia que apuntaba al antiguo primer nodo y Python lo borra de la memoria RAM.
+
+        Args:
+            pos (_type_): La posicion del nodo que se desea eliminar
+        """
         if pos <= 0:
             print(f"ERROR. Por favor inserte una posición mayor a 0")
             return
         pos -= 1 # Línea para que el usuario pueda insertar la posición natural
         if pos == 0: # Si se desea borrar al primer elemento simplemente se reasigna el valor de la cabeza
-            temp = self.head
-            self.head = self.head.ptr
+            temp = self.__head
+            self.__head = self.__head.ptr
             del temp
         else:
             i = 0
-            iterador = self.head
+            iterador = self.__head
             while i < pos - 1 and iterador is not None:
                 iterador = iterador.ptr
                 i += 1
@@ -68,17 +90,50 @@ class ListaEnlazada:
             else:
                 temp = iterador.ptr
                 iterador.ptr = temp.ptr
-                del temp                      
-        
+                del temp    
+                                  
     def imprimir_lista(self):
         
-        i = 0
-        iterador = self.head
+        i = 1
+        iterador = self.__head
         
         while iterador is not None:
-            print(f"({i}){iterador.valor}", end="->")
+            print(f"Posición:[{i}]--VALOR:[{iterador.valor}]", end="===>")
+            iterador = iterador.ptr
+            i += 1
+
+        print("None")    
+        
+    def imprimir_lista_direcciones(self):
+        
+        i = 1
+        iterador = self.__head
+        
+        while iterador is not None:
+            
+            if iterador.ptr is not None:
+                dir = hex(id(iterador))
+                dir2 = hex(id(iterador.ptr))
+            else:
+                dir = hex(id(iterador))
+                dir2 = "None"
+                
+            print(f"Posición:[{i}]--Dirección actual:[{dir}]--VALOR:[{iterador.valor}]--Dirección siguiente:[{dir2}]")
             iterador = iterador.ptr
             i += 1
 
         print("None")
         
+    def verificar_vacio(self):
+        if self.__head is None:
+            print(f"La lista actualmente esta vacía.")
+        else:
+            print(f"Actualmente la lista NO esta vacía.")
+            
+    def imprimir_longitud(self):
+        i = 0
+        iterador = self.__head
+        while iterador is not None:
+            iterador = iterador.ptr
+            i += 1
+        print(f"El tamaño de la lista es de {i} elementos")
--- Python/Didáctico/01_Lista_enlazada/main.py
+++ Python/Didáctico/01_Lista_enlazada/main.py
@@ -2,3 +2,16 @@ from Models.ListaEnlazada import ListaEnlazada
 
 lista = ListaEnlazada()
 
+lista.verificar_vacio()
+lista.insertar_final(10)
+lista.insertar_final(20)
+lista.insertar_final(30)
+lista.insertar_final(40)
+lista.insertar_inicio(80)
+lista.verificar_vacio()
+
+lista.imprimir_longitud()
+
+lista.imprimir_lista()
+
+lista.imprimir_lista_direcciones()
